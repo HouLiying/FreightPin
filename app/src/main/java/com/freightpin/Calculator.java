02c3b739f5c46389186e78c6cd5a6a7f3566a884
HouLiying <h252928054@163.com>
2016-11-22 10:35:48 +0800

    单元测试

---
+++ app/src/main/java/com/freightpin/Calculator.java
@@ -0,0 +1,23 @@
+package com.freightpin;
+
+/**
+ * Created by 侯丽英 on 2016/11/21.
+ */
+
+public class Calculator {
+    public double sum(double a, double b){
+        return a+b;
+    }
+
+    public double substract(double a, double b){
+        return a-b;
+    }
+
+    public double divide(double a, double b){
+        return a/b;
+    }
+
+    public double multiply(double a, double b){
+        return a*b;
+    }
+}
