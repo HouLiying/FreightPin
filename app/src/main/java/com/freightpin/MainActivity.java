1e3c78ca317ce007db381ea25e59802359c6c945
巩凯 <gongkai193521@163.com>
2016-11-22 16:54:58 +0800

    测试

--- app/src/main/java/com/freightpin/MainActivity.java
+++ app/src/main/java/com/freightpin/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.i("----", " == " + "测试测试测试测试测试测试测试测试测试测试");
+        Log.i("----", " == " + "测试测试测试测试测试测试测试测试测试测试111111111");
     }
 }
