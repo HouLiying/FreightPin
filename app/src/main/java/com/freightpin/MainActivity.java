0e8a99488235f80d08ebd5101019d5be0b312ff2
巩凯 <gongkai193521@163.com>
2016-11-22 17:39:29 +0800

    删除测试

--- app/src/main/java/com/freightpin/MainActivity.java
+++ app/src/main/java/com/freightpin/MainActivity.java
@@ -2,7 +2,6 @@ package com.freightpin;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +9,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.i("----", " == " + "测试测试测试测试测试测试测试测试测试测试111111111");
     }
 }
